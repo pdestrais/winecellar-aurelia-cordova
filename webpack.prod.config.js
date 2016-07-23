09bff93d95b4c54583ce7f10cdfc96f044d11d40
Philippe Destrais <philippe.destrais@gmail.com>
2016-07-23 15:56:30 +0200

    fixes to build for android

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -20,6 +20,9 @@ module.exports = {
   plugins: [
     new AureliaWebpackPlugin({
         src: path.resolve('./app/'),
+        includeSubModules: [
+          { moduleId: "aurelia-i18n" }
+        ]
     }),
   ],
   resolve: {
@@ -35,7 +38,8 @@ module.exports = {
       { test: /\.(png|gif|jpg)$/, loader: 'url-loader?limit=8192' },
       { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff2' },
       { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
-      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+      { test: /\.json$/, loader: 'json' }
     ]
   }
 };
